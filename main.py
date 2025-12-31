8c151dfed61b5b46b89df328249319533af7f4f7
Abhilaksh Sharma <abhilaksh.sharma@xcaliber.health>
2025-12-31 14:23:46 -0800

    hello service sample

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from repo-service-1!")
+from service_1.deployment import HelloService
+
+
+if __name__ == "__main__":
+    main()
---
+++ service_1/deployment.py
@@ -0,0 +1,12 @@
+from ray import serve
+
+@serve.deployment(
+    name="hello_service",
+    route_prefix="/hello",
+)
+class HelloService:
+    def __init__(self):
+        print("HelloService initialized")
+
+    async def __call__(self, request):
+        return {"message": "Hello from repo-hello-service"}
