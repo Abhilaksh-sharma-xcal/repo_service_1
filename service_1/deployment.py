40909756359c532ebad83da8be63ecc685c825a8
Abhilaksh-sharma-xcal <abhilaksh.sharma@xcaliber.health>
2026-01-02 14:36:12 +0530

    3 deployments

--- service_1/deployment.py
+++ service_1/deployment.py
@@ -1,9 +1,31 @@
 from ray import serve
 
 @serve.deployment(
-    name="hello_service"
+    name="hello_service_0"
 )
-class HelloService:
+class HelloService0:
+    def __init__(self):
+        print("HelloService initialized")
+
+    async def __call__(self, request):
+        return {"message": "Hello from repo-hello-service"}
+
+
+@serve.deployment(
+    name="hello_service_1"
+)
+class HelloService1:
+    def __init__(self):
+        print("HelloService initialized")
+
+    async def __call__(self, request):
+        return {"message": "Hello from repo-hello-service"}
+
+
+@serve.deployment(
+    name="hello_service_2"
+)
+class HelloService2:
     def __init__(self):
         print("HelloService initialized")
 
